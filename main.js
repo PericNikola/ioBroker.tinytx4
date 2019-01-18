62d640d4d6cb01664f9e9efa97eaf8d0460c3797
Peric Nikola <peric.nikola@ggs.ch>
2019-01-18 18:45:24 +0100

    ...

--- main.js
+++ main.js
@@ -32,6 +32,10 @@
 /*jslint node: true */
 'use strict';
 
+const SerialPort = require("serialport");
+const Readline = SerialPort.parsers.Readline;
+var sp = null;
+
 // you have to require the utils module and call adapter function
 const utils = require('@iobroker/adapter-core'); // Get common adapter utils
 
@@ -42,6 +46,115 @@ const adapterName = require('./package.json').name.split('.').pop();
 it is available then var.The variable is available inside a block and it's childs, but not outside. 
 You can define the same variable name inside a child without produce a conflict with the variable of the parent block.*/
 let variable = 1234;
+
+function write_cmd(command){
+
+    sp.write(command, function(err) {
+        if (err) {
+            return adapter.log.debug('Error on write: ', err.message);
+            }
+        adapter.log.debug('message to USB-stick written : ' + command);
+    });
+}
+
+function definetinytx(id, name){    
+    adapter.setObjectNotExists('tinytx_' + id, {
+        type: 'channel',
+        common: {
+            name: name,
+            role: 'sensor'
+        },
+        native: {
+            "addr": id
+        }
+    });
+    adapter.log.info('RFM12B setting up object = tinytx_' + id);
+
+	adapter.setObjectNotExists('tinytx_' + id + '.voltage', {
+        type: 'state',
+        common: {
+            "name":     "Voltage",
+            "type":     "number",
+            "unit":     "V",
+            "min":      0,
+            "max":      5,
+            "read":     true,
+            "write":    false,
+            "role":     "value.voltage",
+            "desc":     "voltage"
+        },
+        native: {}
+    });
+    adapter.setObjectNotExists('tinytx_' + id + '.temp', {
+        type: 'state',
+        common: {
+            "name":     "Temperature",
+            "type":     "number",
+            "unit":     "°C",
+            "min":      -50,
+            "max":      50,
+            "read":     true,
+            "write":    false,
+            "role":     "value.temperature",
+            "desc":     "Temperature"
+        },
+        native: {}
+    });
+    adapter.setObjectNotExists('tinytx_' + id + '.humid', {
+        type: 'state',
+        common: {
+            "name":     "Humidity",
+            "type":     "number",
+            "unit":     "%",
+            "min":      0,
+            "max":      100,
+            "read":     true,
+            "write":    false,
+            "role":     "value.humidity",
+            "desc":     "Humidity"
+        },
+        native: {}
+    });
+}
+function logtinytx(data){
+    var tmp = data.split(' ');
+	
+	adapter.log.debug('raw Data from Serial Port: ' + data);	//SensorID
+	
+	//adapter.log.debug('Sensor ID: ' + tmp[0]);	//SensorID
+	var SensorID = tmp[0];	//SensorID
+	
+	var MessWerte = tmp[1].split('&');
+	
+	//adapter.log.debug('Batterie Spannung: ' + MessWerte[0].slice(2,6)/1000);  	//Spannung
+	var BattSpannung = 	MessWerte[0].slice(2,6)/1000;  	//Batterie Spannung
+		
+	//adapter.log.debug('Temperatur: ' + MessWerte[1].slice(2,6)/100);			//Temperatur
+	var Temperatur = MessWerte[1].slice(2,6)/100;			//Temperatur
+	
+	//adapter.log.debug('Feuchtigkeit: ' + MessWerte[2].slice(2,6)/100);			//Feuchtigkeit
+	var Feuchtigkeit= MessWerte[2].slice(2,6)/100;			//Feuchtigkeit
+	
+    if(!isNaN(tmp[0])){                      // Wenn ein Datensatz sauber gelesen wurde
+        
+            //var tmpp=tmp.splice(2,8);       // es werden die vorderen Blöcke (0,1) entfernt
+            //adapter.log.debug('splice       : '+ tmpp);
+            //var buf = new Buffer(tmpp);
+            var array=getConfigObjects(adapter.config.sensors, 'sid' , SensorID);
+						
+            if (array.length === 0 || array.length !== 1) {
+            adapter.log.debug('received ID : ' + SensorID + ' is not defined in the adapter Configuration');
+            }	else if (array[0].stype !==  'tinytx'){
+					adapter.log.debug('received ID : ' + SensorID + ' is not defined in the adapter as tinytx ');
+            }	else if (array[0].usid != 'nodef'){
+                						
+                // Werte schreiben
+                adapter.setState('tinytx_'+ array[0].usid +'.humid',    {val: (Feuchtigkeit), ack: true});
+                adapter.setState('tinytx_'+ array[0].usid +'.temp',     {val: (Temperatur), ack: true});
+                adapter.setState('tinytx_'+ array[0].usid +'.voltage',   {val: (BattSpannung), ack: true});                
+            }
+     }
+}
 
 // create adapter instance wich will be used for communication with controller
 let adapter;
